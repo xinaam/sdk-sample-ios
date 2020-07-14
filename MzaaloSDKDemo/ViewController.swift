d9310b99e7556b712929a4299daf7f74977b259c
anush <anush_shenoy@yahoo.co.in>
2020-07-14 12:12:55 +0530

    init
    Signed-off-by: anush <anush_shenoy@yahoo.co.in>

---
+++ MzaaloSDKDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MzaaloSDKDemo
+//
+//  Created by Anush on 14/07/20.
+//  Copyright © 2020 Xfinite. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
