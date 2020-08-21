43fde1a16cbb01185a096abccf8b82b58e3ab66a
Sakthi Nath <sakthinath79@gmail.com>
2020-08-21 13:57:04 +0530

    Build XcodeWorkspace issue fixed

---
+++ Pods/MzaaloAuth/MzaaloSDK.framework/Headers/MzaaloSDK.h
@@ -0,0 +1,19 @@
+//
+//  MzaaloSDK.h
+//  MzaaloSDK
+//
+//  Created by Anush on 28/06/20.
+//  Copyright © 2020 Xfinite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MzaaloSDK.
+FOUNDATION_EXPORT double MzaaloSDKVersionNumber;
+
+//! Project version string for MzaaloSDK.
+FOUNDATION_EXPORT const unsigned char MzaaloSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MzaaloSDK/PublicHeader.h>
+
+
---
+++ Pods/MzaaloRewards/MzaaloRewardsSDK.framework/Headers/MzaaloRewardsSDK.h
@@ -0,0 +1,19 @@
+//
+//  MzaaloRewardsSDK.h
+//  MzaaloRewardsSDK
+//
+//  Created by Anush on 01/07/20.
+//  Copyright © 2020 Xfinite. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MzaaloRewardsSDK.
+FOUNDATION_EXPORT double MzaaloRewardsSDKVersionNumber;
+
+//! Project version string for MzaaloRewardsSDK.
+FOUNDATION_EXPORT const unsigned char MzaaloRewardsSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MzaaloRewardsSDK/PublicHeader.h>
+
+
